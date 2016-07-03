901b5cb6586b91eb1a59a2a4626c02f708b69156
Filchakov Denys <filchakov.denis@gmail.com>
2016-07-03 20:15:36 +0300

    start working

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Prettus\Repository\Providers\RepositoryServiceProvider::class
     ],
 
     /*
